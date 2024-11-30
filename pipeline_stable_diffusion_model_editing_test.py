f2f0e93ddb3cebbd1f35f9a2af2843c0b69d7122
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2024-11-30 23:05:19 +0900

    update test

--- pipeline_stable_diffusion_model_editing_test.py
+++ pipeline_stable_diffusion_model_editing_test.py
@@ -1,5 +1,6 @@
 import pytest
 import torch
+from diffusers import DiffusionPipeline
 
 from pipeline_stable_diffusion_model_editing import StableDiffusionModelEditingPipeline
 
@@ -19,46 +20,56 @@ def device() -> torch.device:
     return torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
-@pytest.fixture
-def source_prompt() -> str:
-    return "A pack of roses"
+class TestPipelineStableDiffusionModelEditing:
+    @pytest.fixture
+    def source_prompt(self) -> str:
+        return "A pack of roses"
 
+    @pytest.fixture
+    def destination_prompt(self) -> str:
+        return "A pack of blue roses"
 
-@pytest.fixture
-def destination_prompt() -> str:
-    return "A pack of blue roses"
+    @pytest.fixture
+    def prompt(self) -> str:
+        return "A field of roses"
 
+    @pytest.fixture
+    def seed(self) -> int:
+        return 19950815
 
-@pytest.fixture
-def prompt() -> str:
-    return "A field of roses"
+    def test_pipeline(
+        self,
+        model_id: str,
+        torch_dtype: torch.dtype,
+        device: torch.device,
+        source_prompt: str,
+        destination_prompt: str,
+        prompt: str,
+        seed: int,
+    ):
+        pipe = StableDiffusionModelEditingPipeline.from_pretrained(
+            model_id,
+            torch_dtype=torch_dtype,
+        )
+        pipe = pipe.to(device)
 
+        pipe.edit_model(
+            source_prompt=source_prompt,
+            destination_prompt=destination_prompt,
+        )
 
-@pytest.fixture
-def seed() -> int:
-    return 19950815
+        output = pipe(prompt=prompt, generator=torch.manual_seed(seed))
+        image = output.images[0]
+        image.save("output.png")
 
-
-def test_pipeline_stable_diffusion_model_editing(
-    model_id: str,
-    torch_dtype: torch.dtype,
-    device: torch.device,
-    source_prompt: str,
-    destination_prompt: str,
-    prompt: str,
-    seed: int,
-) -> None:
-    pipe = StableDiffusionModelEditingPipeline.from_pretrained(
-        model_id,
-        torch_dtype=torch_dtype,
-    )
-    pipe = pipe.to(device)
-
-    pipe.edit_model(
-        source_prompt=source_prompt,
-        destination_prompt=destination_prompt,
-    )
-
-    output = pipe(prompt=prompt, generator=torch.manual_seed(seed))
-    image = output.images[0]
-    image.save("output.png")
+    def test_custom_pipeline(
+        self,
+        model_id: str,
+        torch_dtype: torch.dtype,
+    ):
+        pipe = DiffusionPipeline.from_pretrained(
+            model_id,
+            custom_pipeline="./pipeline_stable_diffusion_model_editing.py",
+            torch_dtype=torch_dtype,
+        )
+        assert pipe.__class__.__name__ == "StableDiffusionModelEditingPipeline"
