2af77c56c2e9a1eb8edd28d4db2a9b3bb16c3008
Shunsuke KITADA <shunsuke.kitada.0831@gmail.com>
2024-11-30 23:52:19 +0900

    update test

--- pipeline_stable_diffusion_model_editing_test.py
+++ pipeline_stable_diffusion_model_editing_test.py
@@ -1,6 +1,9 @@
+import os
+
 import pytest
 import torch
 from diffusers import DiffusionPipeline
+from huggingface_hub import HfApi
 
 from pipeline_stable_diffusion_model_editing import StableDiffusionModelEditingPipeline
 
@@ -20,6 +23,16 @@ def device() -> torch.device:
     return torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
+@pytest.fixture
+def root_dir() -> str:
+    return os.path.dirname(__file__)
+
+
+@pytest.fixture
+def pipeline_script_path(root_dir: str) -> str:
+    return os.path.join(root_dir, "pipeline_stable_diffusion_model_editing.py")
+
+
 class TestPipelineStableDiffusionModelEditing:
     @pytest.fixture
     def source_prompt(self) -> str:
@@ -37,6 +50,10 @@ class TestPipelineStableDiffusionModelEditing:
     def seed(self) -> int:
         return 19950815
 
+    @pytest.mark.skipif(
+        not torch.cuda.is_available(),
+        reason="No GPUs available for testing.",
+    )
     def test_pipeline(
         self,
         model_id: str,
@@ -66,10 +83,51 @@ class TestPipelineStableDiffusionModelEditing:
         self,
         model_id: str,
         torch_dtype: torch.dtype,
+        pipeline_script_path: str,
     ):
         pipe = DiffusionPipeline.from_pretrained(
             model_id,
-            custom_pipeline="./pipeline_stable_diffusion_model_editing.py",
+            custom_pipeline=pipeline_script_path,
             torch_dtype=torch_dtype,
         )
         assert pipe.__class__.__name__ == "StableDiffusionModelEditingPipeline"
+
+
+class TestPushToHub:
+    @pytest.fixture
+    def hf_org_name(self) -> str:
+        return "py-img-gen"
+
+    @pytest.fixture
+    def hf_pipeline_name(self) -> str:
+        return "stable-diffusion-text-to-model-editing"
+
+    @pytest.fixture
+    def hf_repo_id(self, hf_org_name: str, hf_pipeline_name: str) -> str:
+        return f"{hf_org_name}/{hf_pipeline_name}"
+
+    @pytest.fixture
+    def readme_path(self, root_dir: str) -> str:
+        return os.path.join(root_dir, "README.md")
+
+    def test_push_to_hub(
+        self,
+        hf_repo_id: str,
+        pipeline_script_path: str,
+        readme_path: str,
+        repo_type: str = "model",
+    ) -> None:
+        api = HfApi()
+
+        api.upload_file(
+            path_or_fileobj=pipeline_script_path,
+            path_in_repo="pipeline.py",
+            repo_id=hf_repo_id,
+            repo_type=repo_type,
+        )
+        api.upload_file(
+            path_or_fileobj=readme_path,
+            path_in_repo="README.md",
+            repo_id=hf_repo_id,
+            repo_type=repo_type,
+        )
